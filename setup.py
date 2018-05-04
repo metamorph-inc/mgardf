d5f22230aff3689d3c0892b54aa1f91040e62e5b
Kevin Smyth <kevin.m.smyth@gmail.com>
2018-05-04 17:14:42 -0500

    v0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
     name='mgardf',
-    version='0.2.1',
+    version='0.2.2',
     description='Utilities for converting GME MGA structures to RDF',
     long_description=readme,
     author='Adam Nagel',
